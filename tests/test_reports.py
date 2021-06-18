7e1435c6096612bd4decf64b54c62f3c1d597b1b
Peter Vegh <pvegh@posteo.uk>
2021-06-18 19:18:19 +0100

    Remove PyPDF2 from test

--- tests/test_reports.py
+++ tests/test_reports.py
@@ -1,5 +1,4 @@
 import os
-from PyPDF2 import PdfFileReader
 
 import overhang
 
@@ -8,9 +7,6 @@ def test_write_pdf_report(tmpdir):
     pdf_path = os.path.join(str(tmpdir), "test_report.pdf")
     overhangs = overhang.generate_all_overhangs(3)
     overhang.write_pdf_report(target=pdf_path, overhangs=overhangs)
-    with open(pdf_path, "rb") as f:
-        reader = PdfFileReader(f)
-        assert reader.numPages == 34
 
     with open(pdf_path, "rb") as f:
         filesize = len(f.read())
