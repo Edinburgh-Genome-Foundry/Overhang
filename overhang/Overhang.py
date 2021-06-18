d72eab790010ec54c4e936154f04c346eb13b782
Peter Vegh <pvegh@posteo.uk>
2021-06-18 16:12:28 +0100

    Add docstrings
    Refactor has_trimer to has_multimer.

--- overhang/Overhang.py
+++ overhang/Overhang.py
@@ -5,6 +5,19 @@ from .tools import order_overhangs, generate_overhang_pairs
 
 
 class Overhang:
+    """Class for an overhang and its reverse complement.
+
+    Note that the overhang with the lower order (e.g. AATA < TATT) will be stored in
+    `Overhang.overhang` and the reverse complement in `Overhang.overhang_rc`, regardless
+    of which was given as parameter.
+
+
+    **Parameters**
+
+    **seq**
+    > ACGT sequence (`str`).
+    """
+
     def __init__(self, seq):
         self.overhang, self.overhang_rc = order_overhangs(seq)
         if len(set([self.overhang, self.overhang_rc])) == 1:
@@ -18,16 +31,29 @@ class Overhang:
         self.find_codons()
 
     def is_good(self):
+        """Summarise attributes and decide whether overhang can be used for assembly."""
         return not any([self.is_palindromic])
 
     def count_max_repeat(self, repeat=3):
+        """Check overhang for repeating letters.
+
+
+        **Parameters**
+
+        **repeat**
+        > Number of minimum repeats to flag (`int`). For example, 3 checks for AAA etc.
+        """
         for letter in "ATCG":
             if self.overhang.count(letter * repeat) != 0:
-                self.has_trimer = True
+                self.has_multimer = True
                 return
-        self.has_trimer = False
+        self.has_multimer = False
 
     def find_codons(self):
+        """Check overhang for presence of start and stop codons.
+
+        This is important information on the suitability of an overhang.
+        """
         start = ["ATG"]
         stop = ["TAA", "TAG", "TGA"]
         for codon in start:
@@ -56,6 +82,14 @@ class Overhang:
 
 
 def generate_all_overhangs(overhang_length=4):
+    """Generate list Overhang class instances for all overhangs of given length.
+
+
+    **Parameters**
+
+    **overhang_length**
+    > Length of overhangs (`int`).
+    """
     overhang_pairs = generate_overhang_pairs(overhang_length=overhang_length)
     overhang_strings = [next(iter(overhang_pair)) for overhang_pair in overhang_pairs]
     overhang_strings.sort()
--- overhang/tools.py
+++ overhang/tools.py
@@ -3,12 +3,29 @@ import goldenhinges
 
 
 def order_overhangs(seq):
+    """Create an overhang's reverse complement, and return an ordered list of them.
+
+    Overhangs are ordered by the letters, e.g. AATA < TATT.
+
+
+    **Parameters**
+
+    **seq**
+    > ACGT sequence (`str`)."""
     sorted_overhangs = [seq, goldenhinges.reverse_complement(seq)]
     sorted_overhangs.sort()
     return sorted_overhangs[0], sorted_overhangs[1]
 
 
 def generate_overhang_pairs(overhang_length=4):
+    """Generate all overhang pairs of given length.
+
+
+    **Parameters**
+
+    **overhang_length**
+    > Length of overhangs (`int`).
+    """
     raw_overhangs = [
         "".join(overhang)
         for overhang in itertools.product(*overhang_length * ("ACGT",))
