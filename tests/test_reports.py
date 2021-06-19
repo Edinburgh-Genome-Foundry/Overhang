bb041f8703eab9121b7649dd3f87c4b46d6a91b5
Peter Vegh <pvegh@posteo.uk>
2021-06-19 16:00:58 +0100

    Test plots in report

--- tests/test_reports.py
+++ tests/test_reports.py
@@ -5,9 +5,9 @@ import overhang
 
 def test_write_pdf_report(tmpdir):
     pdf_path = os.path.join(str(tmpdir), "test_report.pdf")
-    overhangs = overhang.generate_all_overhangs(3)
-    overhang.write_pdf_report(target=pdf_path, overhangs=overhangs)
+    overhangs = overhang.generate_all_overhangs(4)
+    overhang.write_pdf_report(target=pdf_path, overhangs=overhangs[0:10])
 
     with open(pdf_path, "rb") as f:
         filesize = len(f.read())
-        assert filesize > 100000
+        assert filesize > 300000
