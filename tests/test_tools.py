425ed7ce90cd76712528439aaedf18ea59371afc
Peter Vegh <pvegh@posteo.uk>
2021-07-02 17:08:58 +0100

    Test filter_overhangs()

--- tests/test_tools.py
+++ tests/test_tools.py
@@ -10,3 +10,18 @@ def test_generate_overhang_pairs():
         frozenset({"C", "G"}),
         frozenset({"A", "T"}),
     }
+
+
+def test_filter_overhangs():
+    overhangs = [
+        overhang.Overhang(ovhg)
+        for ovhg in [
+            "TAGG",
+            "ATGG",  # Good overhangs
+            "CACC",  # Weakly annealing
+            "GACT",  # Self misannealing
+        ]
+    ]
+    filtered_overhangs = overhang.filter_overhangs(overhangs)
+    assert len(filtered_overhangs) == 2
+    assert set([ovhg.overhang for ovhg in filtered_overhangs]) == set(["CCTA", "ATGG"])
