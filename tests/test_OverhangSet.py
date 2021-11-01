0da0953a47e50a713302cc15f528a28fe3902c38
Peter Vegh <pvegh@posteo.uk>
2021-11-01 12:02:00 +0000

    Test overhangs in enzyme sites

--- tests/test_OverhangSet.py
+++ tests/test_OverhangSet.py
@@ -14,3 +14,9 @@ def test_OverhangSet():
     overhangset = overhang.OverhangSet(overhangs=["CTAT", "GGAC", "TGTT"], name="Test")
     overhangset.find_perfect_subset()
     assert set(overhangset.subset) == set(["TGTT", "CTAT"])
+
+    # Test overhangs in enzyme sites. GTCT in BsmBI site, GACG in revcomp site:
+    overhangset = overhang.OverhangSet(overhangs=["ATGC", "GTCT", "GACG"], name="Test")
+    overhangset.inspect_overhangs()
+    assert overhangset.has_warnings
+    assert overhangset.overhangs_in_site_txt == "AGAC/GTCT; CGTC/GACG"
